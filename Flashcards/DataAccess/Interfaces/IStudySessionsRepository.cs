feb79f33af19e3d7b3eefb31b5741869c9ecd3fe
Dejmenek <dejmen14567@gmail.com>
2025-07-22 21:26:30 +0200

    feat: Update IStudySessionsRepository to use Result<T>
    The IStudySessionsRepository interface has been modified to return
    Result<T> types instead of direct Task<T> types for its methods.
    This change improves error handling and provides a more structured
    way to return results. The following methods were updated:
    - GetMonthlyStudySessionReportAsync
    - GetMonthlyStudySessionAverageScoreReportAsync
    - AddStudySessionAsync
    - GetAllStudySessionsAsync
    - HasStudySessionAsync

--- Flashcards/DataAccess/Interfaces/IStudySessionsRepository.cs
+++ Flashcards/DataAccess/Interfaces/IStudySessionsRepository.cs
@@ -1,12 +1,13 @@
 ﻿using Flashcards.Models;
+using Flashcards.Utils;
 
 namespace Flashcards.DataAccess.Interfaces;
 
 public interface IStudySessionsRepository
 {
-    Task<IEnumerable<MonthlyStudySessionsNumberData>> GetMonthlyStudySessionReportAsync(string year);
-    Task<IEnumerable<MonthlyStudySessionsAverageScoreData>> GetMonthlyStudySessionAverageScoreReportAsync(string year);
-    Task AddStudySessionAsync(int stackId, DateTime date, int score);
-    Task<IEnumerable<StudySession>> GetAllStudySessionsAsync();
-    Task<bool> HasStudySessionAsync();
+    Task<Result<IEnumerable<MonthlyStudySessionsNumberData>>> GetMonthlyStudySessionReportAsync(string year);
+    Task<Result<IEnumerable<MonthlyStudySessionsAverageScoreData>>> GetMonthlyStudySessionAverageScoreReportAsync(string year);
+    Task<Result> AddStudySessionAsync(int stackId, DateTime date, int score);
+    Task<Result<IEnumerable<StudySession>>> GetAllStudySessionsAsync();
+    Task<Result<bool>> HasStudySessionAsync();
 }
