485ab8d10829944a5f89496cb5a882f35df0470e
Dejmenek <dejmen14567@gmail.com>
2025-07-30 14:08:16 +0200

    feat: add BaseIntegrationTest class for integration tests
    Introduces a new `BaseIntegrationTest` class in the
    `Flashcards.IntegrationTests` namespace. This class implements
    the `IClassFixture<TestClassFixture>` and `IDisposable`
    interfaces, providing a constructor to initialize a service
    scope and a `DataContext` using the service provider's
    configuration. It also includes an `InitializeDatabaseAsync`
    method for asynchronous database initialization and a
    `Dispose` method for proper resource cleanup after tests.

---
+++ Flashcards.IntegrationTests/BaseIntegrationTest.cs
@@ -0,0 +1,21 @@
+﻿using Flashcards.DataAccess;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Flashcards.IntegrationTests;
+public class BaseIntegrationTest : IClassFixture<TestClassFixture>, IDisposable
+{
+    protected readonly IServiceScope _scope;
+    protected readonly DataContext _dbContext;
+
+    protected BaseIntegrationTest(TestClassFixture fixture)
+    {
+        _scope = fixture.TestHost.Services.CreateScope();
+        var config = _scope.ServiceProvider.GetRequiredService<IConfiguration>();
+        _dbContext = new DataContext(config);
+    }
+
+    public void Dispose() => _scope?.Dispose();
+
+    protected async Task InitializeDatabaseAsync() => await _dbContext.Init();
+}
