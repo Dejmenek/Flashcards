b13ec5bd463d456cea23e38d2ce82a61f431b636
Dejmenek <dejmen14567@gmail.com>
2025-07-31 17:32:14 +0200

    test: add integration tests for StudySessionsService
    Introduces the `StudySessionsServiceIntegrationTests` class in the `Flashcards.IntegrationTests` namespace. This class tests the functionality of the `StudySessionsService`, ensuring that study sessions can be created successfully with valid flashcards. It includes necessary service initializations and implements the `IAsyncLifetime` interface for proper setup and teardown. The test method verifies the success of the operation and checks that the stored study sessions are correctly associated with the expected stack ID.

---
+++ Flashcards.IntegrationTests/StudySessionsServiceIntegrationTests.cs
@@ -0,0 +1,49 @@
+﻿using Flashcards.DataAccess.Interfaces;
+using Flashcards.Helpers;
+using Flashcards.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
+
+namespace Flashcards.IntegrationTests;
+public class StudySessionsServiceIntegrationTests : BaseIntegrationTest, IAsyncLifetime
+{
+    private readonly IStudySessionsService _studySessionsService;
+    private readonly IStudySessionsRepository _studySessionsRepository;
+    private readonly IUserInteractionService _userInteractionService;
+    private readonly IStacksRepository _stacksRepository;
+
+    public StudySessionsServiceIntegrationTests(TestClassFixture fixture) : base(fixture)
+    {
+        _studySessionsRepository = _scope.ServiceProvider.GetRequiredService<IStudySessionsRepository>();
+        _studySessionsService = _scope.ServiceProvider.GetRequiredService<IStudySessionsService>();
+        _userInteractionService = _scope.ServiceProvider.GetRequiredService<IUserInteractionService>();
+        _stacksRepository = _scope.ServiceProvider.GetRequiredService<IStacksRepository>();
+    }
+
+    public Task DisposeAsync() => Task.CompletedTask;
+
+    public async Task InitializeAsync() => await InitializeDatabaseAsync();
+
+    [Fact]
+    public async Task RunStudySessionAsync_WithValidFlashcards_AddsStudySessionAndReturnsSuccess()
+    {
+        // Arrange
+        var stackId = 3;
+        var flashcards = (await _stacksRepository.GetFlashcardsByStackIdAsync(stackId)).Value;
+        var flashcardDTOs = flashcards.Select(Mapper.ToFlashcardDTO).ToList();
+
+        _userInteractionService.GetAnswer().Returns("Good morning", "Goodbye", "Please");
+
+        // Act
+        var result = await _studySessionsService.RunStudySessionAsync(flashcardDTOs, stackId);
+
+        // Assert
+        Assert.True(result.IsSuccess);
+
+        var studySessionsResult = await _studySessionsRepository.GetAllStudySessionsAsync();
+        Assert.True(studySessionsResult.IsSuccess);
+        Assert.NotEmpty(studySessionsResult.Value);
+        Assert.NotNull(studySessionsResult.Value);
+        Assert.Equal(stackId, studySessionsResult.Value.First().StackId);
+    }
+}
