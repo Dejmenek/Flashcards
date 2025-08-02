b4982e76740e74ffd07f297cfe866e7a0d246635
Dejmenek <dejmen14567@gmail.com>
2025-08-02 23:27:04 +0200

    feat: add new card classes and DTOs
    Refactored the `Flashcard` class to inherit from a new abstract class `BaseCard`, which consolidates common properties like `Id`, `StackId`, and `CardType`. The `Front` and `Back` properties have been moved to the derived `Flashcard` class, which initializes `CardType` in its constructor.
    
    Added new subclasses for different card types: `ClozeCard`, `FillInCard`, and `MultipleChoiceCard`, each with specific properties and constructors. Created a `BaseCardDTO` class to reflect the structure of `BaseCard`, and added corresponding DTO classes for each card type. Introduced an enumeration `CardType` to define the various card types in the application.

--- Flashcards/Models/Flashcard.cs
+++ Flashcards/Models/Flashcard.cs
@@ -1,16 +1,74 @@
 ﻿namespace Flashcards.Models;
 
-public class Flashcard
+public abstract class BaseCard
 {
     public int Id { get; set; }
     public int StackId { get; set; }
-    public string Front { get; set; }
-    public string Back { get; set; }
+    public CardType CardType { get; set; }
 }
 
-public record class FlashcardDTO
+public class Flashcard : BaseCard
+{
+    public string? Front { get; set; }
+    public string? Back { get; set; }
+    public Flashcard() => CardType = CardType.Flashcard;
+}
+
+public class ClozeCard : BaseCard
+{
+    public string? ClozeText { get; set; }
+    public ClozeCard() => CardType = CardType.Cloze;
+}
+
+public class FillInCard : BaseCard
+{
+    public string? FillInText { get; set; }
+    public List<string>? Answer { get; set; }
+    public FillInCard() => CardType = CardType.FillIn;
+}
+
+public class MultipleChoiceCard : BaseCard
+{
+    public string? Question { get; set; }
+    public List<string>? Choices { get; set; }
+    public string? Answer { get; set; }
+    public MultipleChoiceCard() => CardType = CardType.MultipleChoice;
+}
+
+public abstract record class BaseCardDTO
 {
     public int Id { get; init; }
-    public string Front { get; init; }
-    public string Back { get; init; }
+    public CardType CardType { get; init; }
+}
+
+public record class FlashcardDTO : BaseCardDTO
+{
+    public string Front { get; init; } = string.Empty;
+    public string Back { get; init; } = string.Empty;
+}
+
+public record class ClozeCardDTO : BaseCardDTO
+{
+    public string ClozeText { get; init; } = string.Empty;
+}
+
+public record class FillInCardDTO : BaseCardDTO
+{
+    public string FillInText { get; init; } = string.Empty;
+    public List<string> Answer { get; init; } = new();
+}
+
+public record class MultipleChoiceCardDTO : BaseCardDTO
+{
+    public string Question { get; init; } = string.Empty;
+    public List<string> Choices { get; init; } = new();
+    public string Answer { get; init; } = string.Empty;
+}
+
+public enum CardType
+{
+    Flashcard,
+    Cloze,
+    FillIn,
+    MultipleChoice
 }
