204e502cd5f7025a84142444d38e89ad8ed09b4f
Dejmenek <dejmen14567@gmail.com>
2025-08-04 15:50:30 +0200

    feat: integrate Serilog for enhanced logging
    Updated `Flashcards.csproj` to include Serilog and its extensions for improved logging capabilities. Modified `Program.cs` to configure logging with Serilog, utilizing a new configuration object to read from `appsettings.json` and environment variables. The logger is set to write logs to a SQL Server database, ensuring consistent logging throughout the application by using `UseSerilog()` in the host builder.

--- Flashcards/Program.cs
+++ Flashcards/Program.cs
@@ -8,22 +8,45 @@ using Flashcards.Services.Interfaces;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Sinks.MSSqlServer;
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(AppContext.BaseDirectory)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables()
+    .Build();
+
+var connectionString = config.GetConnectionString("Default");
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo
+    .MSSqlServer(
+        connectionString: connectionString,
+        new MSSqlServerSinkOptions
+        {
+            TableName = "Logs"
+        }
+    ).CreateLogger();
 
 var host = Host.CreateDefaultBuilder(args)
-        .ConfigureHostConfiguration(hostConfig =>
+        .ConfigureAppConfiguration((context, configBuilder) =>
         {
-            hostConfig.SetBasePath(AppContext.BaseDirectory);
-            hostConfig.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-            hostConfig.AddEnvironmentVariables();
+            configBuilder.AddConfiguration(config);
         })
         .ConfigureServices((hostContext, services) =>
         {
-            var connectionString = hostContext.Configuration.GetConnectionString("Default");
+            var connectionString = config.GetConnectionString("Default");
 
             if (string.IsNullOrWhiteSpace(connectionString))
             {
                 throw new InvalidOperationException("The connection string 'Default' is not configured or is empty. Please check your appsettings.json file.");
             }
+
+            services.AddLogging(builder =>
+            {
+                builder.AddSerilog(Log.Logger);
+            });
 
             services.AddSingleton<IUserInteractionService, UserInteractionService>();
             services.AddSingleton<IConsoleService, ConsoleService>();
@@ -38,9 +61,9 @@ var host = Host.CreateDefaultBuilder(args)
             services.AddSingleton<CardsController>();
             services.AddScoped<Menu>();
         })
+        .UseSerilog()
         .Build();
 
-var config = host.Services.GetRequiredService<IConfiguration>();
 var dataContext = new DataContext(config);
 await dataContext.Init();
 
