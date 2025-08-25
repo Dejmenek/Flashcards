18e39aa38c113014d97526d5461d3e908224639f
Dejmenek <dejmen14567@gmail.com>
2025-08-25 14:22:05 +0200

    chore: seed cloze deletion cards

--- Flashcards/SqlScripts/SeedCards.sql
+++ Flashcards/SqlScripts/SeedCards.sql
@@ -1,4 +1,4 @@
-﻿INSERT INTO Cards (StackId, Front, Back, CardType) VALUES
+INSERT INTO Cards (StackId, Front, Back, CardType) VALUES
 	(1, 'Hola', 'Hello', 'Flashcard'),
 	(1, '¿Cómo estás?', 'How are you?', 'Flashcard'),
 	(1, 'Gracias', 'Thank you', 'Flashcard'),
@@ -9,7 +9,13 @@
 	(3, 'Do widzenia', 'Goodbye', 'Flashcard'),
 	(3, 'Proszę', 'Please', 'Flashcard');
 
-	INSERT INTO Cards (StackId, Question, Choices, Answer, CardType) VALUES
-	(3, 'Jakie jest największe miasto w Polsce?', 'Kraków;Warszawa;Gdańsk;Wrocław', 'Warszawa', 'MultipleChoice'),
+INSERT INTO Cards (StackId, Question, Choices, Answer, CardType) VALUES
+    (3, 'Jakie jest największe miasto w Polsce?', 'Kraków;Warszawa;Gdańsk;Wrocław', 'Warszawa', 'MultipleChoice'),
 	(3, 'Które z tych są polskimi rzekami?', 'Wisła;Odra;Ren;Dunaj;Warta', 'Wisła;Odra;Warta', 'MultipleChoice'),
 	(3, 'Ile województw ma Polska?', 'Czternaście;Piętnaście;Szesnaście;Siedemnaście', 'Szesnaście', 'MultipleChoice');
+
+INSERT INTO Cards (StackId, ClozeText, CardType) VALUES
+    (2, 'Guten {{c1::Morgen}}!', 'Cloze'),
+    (2, 'Ich {{c1::heiße}} Anna.', 'Cloze'),
+    (2, 'Wie {{c1::geht}} es {{c2::dir}}?', 'Cloze'),
+    (2, 'Ich komme aus {{c1::Deutschland}}.', 'Cloze');
